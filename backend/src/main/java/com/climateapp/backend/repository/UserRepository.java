0d6afd048d58bb472a1fee2f3555a1dff8a3ddb9
Henna <henna.hannila@gmail.com>
2023-03-22 16:07:59 +0200

    pientä päivitystä, authorisoinnin kanss ongelmaa

--- backend/src/main/java/com/climateapp/backend/repository/UserRepository.java
+++ backend/src/main/java/com/climateapp/backend/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.climateapp.backend.repository;
 
-import com.climateapp.backend.data.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.climateapp.backend.data.Users;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserAccount, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 
-    UserAccount findByUsername(String username);
 }
+
