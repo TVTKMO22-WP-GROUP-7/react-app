60b3d714f41358682a2a8dff5e815d1c118e4c45
tommi20128 <70171019+tommi20128@users.noreply.github.com>
2023-03-29 11:45:05 +0300

    Lisätty toimiva Navbar fronttiin

--- frontend/src/components/DefaultView.js
+++ frontend/src/components/DefaultView.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Navbar from './Navbar'
 
 export default function DefaultView() {
     return (
-        <div className="default">
-            <div className="navbar">
-                <div><Link to="/changepassword"><button>Change Password</button></Link></div>
-                <div><Link to="/deleteaccount"><button>Delete Account</button></Link></div>
-                <div><Link to="/"><button>Logout</button></Link></div>
-            </div>
+    <>
+    <Navbar/>
+    <div className="default">
             <p>This is the default view user gets when logged in</p>
             <p>Tähän alle näkymät V1 ja V2 allekkain ??</p>
         </div>
-    )
+    </>      
+    );
 }
