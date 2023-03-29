1fd803a2edbfbf71e1cf0632f1bc2b270868f9e1
Haudihou <newasoft@hotmail.com>
2023-03-29 11:12:06 +0300

    Sovellus ajantasalla, login kesken

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -7,73 +7,69 @@ export default function Login() {
 
     const navigate = useNavigate();
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    console.log(event.target.username.value);
-    console.log(event.target.password.value);
-    console.log(
-        "/login?username=" +
-        event.target.username.value +
-        "&password=" +
-        event.target.password.value
-    );
-
-
-    try {
-      const result = await axios.post(Constants.API_ADDRESS+ "/login?username=" + event.target.username.value + "&password=" + event.target.password.value+".");
-      console.log(result);
-      navigate("/", {replace:true});
-    } catch (error) {
-      console.log(error.response);
-      if (error.response && error.response.status === 403) {
-        setError({username: error.response.data});
-      } else {
-        console.log(error);
-      }
-    }
-  };
-
     const [input, setInput] = useState({
         username: '',
         password: '',
-      });
-    
-      const [error, setError] = useState({
+    });
+
+    const [error, setError] = useState({
         username: '',
         password: '',
-      });
-    
-      const onInputChange = e => {
+    });
+
+    const onInputChange = e => {
         const { name, value } = e.target;
         setInput(prev => ({
-          ...prev,
-          [name]: value
+            ...prev,
+            [name]: value
         }));
         validateInput(e);
-      }
-      const validateInput = e => {
+    }
+
+    const validateInput = e => {
         let { name, value } = e.target;
         setError(prev => {
-          const stateObj = { ...prev, [name]: '' };
-    
-          switch (name) {
-            case "username":
-              if (!value) {
-                stateObj[name] = "Please enter username";
-              }
-              break;
-            case "password":
-              if (!value) {
-                stateObj[name] = "Please enter password";
-              }
-              break;
+            const stateObj = { ...prev, [name]: '' };
 
-            default:
-              break;
-          }
-          return stateObj;
+            switch (name) {
+                case "username":
+                    if (!value) {
+                        stateObj[name] = "Please enter username";
+                    }
+                    break;
+                case "password":
+                    if (!value) {
+                        stateObj[name] = "Please enter password";
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            return stateObj;
         });
-      }
+    }
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post(Constants.API_ADDRESS + "/login", {
+                username: input.username,
+                password: input.password,
+            });
+
+            if (response.data.success) {
+                // The user is authenticated, navigate to the default view
+                navigate("/", { replace: true });
+            } else {
+                // The user is not authenticated, display an error message
+                setError({ username: "Invalid username or password" });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <div style={{ border: '1px solid black', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}> 
@@ -82,16 +78,16 @@ export default function Login() {
             <div className="login-container">
                 <div className="login">
                     <h4>Login</h4>
-                    <form onSubmit = {handleLogin}>
-                    <input type="text" name="username" placeholder="Enter username" value={input.username} onChange={onInputChange} onBlur={validateInput}></input>
-                    {error.username && <span className="err">{error.username} </span>}
-                    <input type="password" name="password" placeholder="Enter password" value={input.password} onChange={onInputChange} onBlur={validateInput}></input>
-                    {error.password && <span className="err">{error.password} </span>}
+                    <form onSubmit={handleLogin}>
+                        <input type="text" name="username" placeholder="Enter username" value={input.username} onChange={onInputChange} onBlur={validateInput}></input>
+                        {error.username && <span className="err">{error.username} </span>}
+                        <input type="password" name="password" placeholder="Enter password" value={input.password} onChange={onInputChange} onBlur={validateInput}></input>
+                        {error.password && <span className="err">{error.password} </span>}
+                        <button type="submit">Log in</button>
                     </form>
                 </div>
-                </div>
-                <br/>
-            <Link to="defaultview"><button>Log in</button></Link>
+            </div>
+            <br/>
             <Link to="register"><button>Register here</button></Link>     
         </div>
     )
