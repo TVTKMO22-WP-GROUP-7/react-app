4e5911d52f5b081b6afb7b451879a94e3fbe1ed0
Henna <henna.hannila@gmail.com>
2023-04-04 17:50:50 +0300

    V5 väännetty

--- frontend/src/components/visualisations/V5.js
+++ frontend/src/components/visualisations/V5.js
@@ -4,6 +4,9 @@ import { Chart as ChartJS } from "chart.js/auto";
 import "chartjs-adapter-moment";
 import Constants from "../Constants.json";
 
+import './Visu.css';
+
+
 function V5() {
 
     const [data1, setData] = useState([]);
@@ -11,14 +14,13 @@ function V5() {
     const [industrialData, setIndustrialData] = useState([]);
     const [wasteData, setWasteData] = useState([]);
     const [afoluData, setAfoluData] = useState([]);
-    const [data2, setData2] = useState([]);
-    const [data3, setData3] = useState([]);
-
+    const [selectedSector, setSelectedSector] = useState();
 
     useEffect(() => {
         fetch(Constants.API_ADDRESS + "/v5all")
             .then(response => response.json())
             .then(data => {
+                setSelectedSector({ data: data, label: "All sectors" })
                 console.log(data);
                 setData(data);
             }
@@ -65,49 +67,15 @@ function V5() {
             );
     }, []);
 
-
-
-   /* useEffect(() => {
-        fetch(Constants.API_ADDRESS + "/v5sub")
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setData2(data);
-            }
-            );
-    }, []);*/
-
     useEffect(() => {
-        fetch(Constants.API_ADDRESS + "/v5subfurther")
-
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setData3(data);
-            }
-            );
+        setSelectedSector({ data: data1, label: "All sectors" });
     }, []);
 
     const data = {
-        labels: data1.map(item => item.sector),
+        labels: selectedSector ? selectedSector.data.map(item => item.sector) : [],
         datasets: [
             {
-                data: data1.map(item => item.share),
-                backgroundColor: [
-                    "#9ACD32",
-                    "#ADFF2F",
-                    "#228B22",
-                    "#90EE90",
-                ],
-            }
-        ]
-    }
-
-    const energy = {
-        labels: energyData.map(label => label.sector),
-        datasets: [
-            {
-                data: energyData.map(label => label.share),
+                data: selectedSector ? selectedSector.data.map(item => item.share) : [],
                 backgroundColor: [
                     "#9ACD32",
                     "#ADFF2F",
@@ -130,160 +98,15 @@ function V5() {
                 ],
             }
         ]
-    }
-
-    const industrial = {
-        labels: industrialData.map(label => label.sector),
-        datasets: [
-            {
-                data: industrialData.map(label => label.share),
-                backgroundColor: [
-                    "#9ACD32",
-                    "#ADFF2F",
-                    "#90EE90",
-                    "#00FA9A",
-                    "#00FF7F",
-                    "#3CB371",
-                    "#2E8B57",
-                    "#008000",
-                    "#00FFFF",
-                    "#00CED1",
-                    "#48D1CC",
-                    "#00BFFF",
-                    "#7B68EE",
-                    "#B0C4DE",
-                    "#4682B4",
-                    "#4169E1",
-                    "#0000FF",
-                    "#0000FF",
-                ],
-            }
-        ]
-    }
-
-
-    const waste = {
-        labels: wasteData.map(label => label.sector),
-        datasets: [
-            {
-                data: wasteData.map(label => label.share),
-                backgroundColor: [
-                    "#9ACD32",
-                    "#ADFF2F",
-                    "#90EE90",
-                    "#00FA9A",
-                    "#00FF7F",
-                    "#3CB371",
-                    "#2E8B57",
-                    "#008000",
-                    "#00FFFF",
-                    "#00CED1",
-                    "#48D1CC",
-                    "#00BFFF",
-                    "#7B68EE",
-                    "#B0C4DE",
-                    "#4682B4",
-                    "#4169E1",
-                    "#0000FF",
-                    "#0000FF",
-                ],
-            }
-        ]
-    }
-
-    const afolu = {
-        labels: afoluData.map(label => label.sector),
-        datasets: [
-            {
-                data: afoluData.map(label => label.share),
-                backgroundColor: [
-                    "#9ACD32",
-                    "#ADFF2F",
-                    "#90EE90",
-                    "#00FA9A",
-                    "#00FF7F",
-                    "#3CB371",
-                    "#2E8B57",
-                    "#008000",
-                    "#00FFFF",
-                    "#00CED1",
-                    "#48D1CC",
-                    "#00BFFF",
-                    "#7B68EE",
-                    "#B0C4DE",
-                    "#4682B4",
-                    "#4169E1",
-                    "#0000FF",
-                    "#0000FF",
-                ],
-            }
-        ]
-    }
-
-
-
-    /*const sub = {
-        labels: data2.map(label => label.sector),
-        datasets: [
-            {
-                data: data2.map(label => label.share),
-                backgroundColor: [
-                    "#9ACD32",
-                    "#ADFF2F",
-                    "#90EE90",
-                    "#00FA9A",
-                    "#00FF7F",
-                    "#3CB371",
-                    "#2E8B57",
-                    "#008000",
-                    "#00FFFF",
-                    "#00CED1",
-                    "#48D1CC",
-                    "#00BFFF",
-                    "#7B68EE",
-                    "#B0C4DE",
-                    "#4682B4",
-                    "#4169E1",
-                    "#0000FF",
-                ],
-            }
-        ]
-    }*/
-
-    const subf = {
-        labels: data3.map(label => label.sector),
-        datasets: [
-            {
-                data: data3.map(label => label.share),
-                backgroundColor: [
-                    "#9ACD32",
-                    "#ADFF2F",
-                    "#90EE90",
-                    "#00FA9A",
-                    "#00FF7F",
-                    "#3CB371",
-                    "#2E8B57",
-                    "#008000",
-                    "#00FFFF",
-                    "#00CED1",
-                    "#48D1CC",
-                    "#00BFFF",
-                    "#7B68EE",
-                    "#B0C4DE",
-                    "#4682B4",
-                    "#4169E1",
-                    "#0000FF",
-                ],
-            }
-        ]
-    }
+    };
 
     const options = {
         responsive: true,
         aspectRatio: 5,
         elements: {
             arc: {
-                borderWidth: 0
+                borderWidth: 0,
+                hoverOffset: 20,
             }
         },
         plugins: {
@@ -301,20 +124,34 @@ function V5() {
             text: "CO2 emissions by sectors",
             font: {
                 size: 20,
-            }
-        }
+            },
+            className: 'chart-title',
+        },
     }
 
     return (
-        <div className="child">
-            <div className="container-fluid" width={1000}>
-                <Doughnut data={data} options={options} height={400} width={600} />
-                <Doughnut data={energy} options={options} height={400} width={600} />
-                <Doughnut data={industrial} options={options} height={400} width={600} />
-                <Doughnut data={waste} options={options} height={400} width={600} />
-                <Doughnut data={afolu} options={options} height={400} width={600} />
-                <Doughnut data={subf} options={options} height={400} width={600} />
+        <div className="button-container">
+            <button onClick={() => setSelectedSector({ data: data1, label: "All sectors" })}>All sectors</button>
+            <button onClick={() => setSelectedSector({ data: energyData, label: "Energy" })}>Energy</button>
+            <button onClick={() => setSelectedSector({ data: industrialData, label: "Industrial Processes and Product Use" })}>Industrial Processes and Product Use</button>
+            <button onClick={() => setSelectedSector({ data: wasteData, label: "Waste" })}>Waste</button>
+            <button onClick={() => setSelectedSector({ data: afoluData, label: "Agriculture, Forestry & Land Use" })}>AFOLU</button>
+            <div className="containter-fluid">
+                <h2>{selectedSector ? selectedSector.label : "All sectors"}</h2>
             </div>
+            <div className="container-fluid">
+                <Doughnut data={data} options={options} />
+            </div>
+            <div className="card mt-4" style={{ width: "24rem" }}>
+                <div className="card-body">
+                    <h5 className="card-title>">Description</h5>
+                    <p ClassName="card-text">DESCRIPTION HERE</p>
+                </div>
+                <h6 className="card-subtitle mb-2 text-muted">Sources: </h6>
+                <p> <a href="https://ourworldindata.org/emissions-by-sector#co2-emissions-by-sector" target="_blank" rel="noopener noreferrer" className="card-link">Description</a></p>
+                <p> <a href="https://ourworldindata.org/uploads/2020/09/Global-GHG-Emissions-by-sector-based-on-WRI-2020.xlsx" target="_blank" rel="noopener noreferrer" className="card-link">Dataset</a></p>
+            </div>
+
         </div>
     );
 } export default V5;
