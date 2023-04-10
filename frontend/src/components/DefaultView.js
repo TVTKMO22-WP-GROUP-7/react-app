786b5288e630fa305fed8854d839bbe40fed07ea
Henna <henna.hannila@gmail.com>
2023-04-10 16:49:25 +0300

    merge issues

--- frontend/src/components/DefaultView.js
+++ frontend/src/components/DefaultView.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import V1 from "./visualisations/V1";
 import V2 from "./visualisations/V2";
-import V3 from './visualisations/V3';
+import V3 from "./visualisations/V3";
 
 export default function DefaultView() {
   return (
-    <>
-      <div>
-        <V1 />
-        <V2 />
-        <V3 />
-      </div>
-    </>
+    <div className="grid-container">
+
+      <V1 />
+      <V2 />
+      <V3 />
+    </div>
   );
 }
-
-
-
--- frontend/src/components/DefaultView2.js
+++ frontend/src/components/DefaultView2.js
@@ -1,17 +1,18 @@
-import React from 'react';
 
+import React from 'react';
 import V4 from './visualisations/V4';
 import V5 from './visualisations/V5';
 
 export default function DefaultView2() {
 
   return (
-    <>
-      <div>
-
+    <div className="grid-container">
+      <div className="grid-item">
         <V4 />
+      </div>
+      <div className="grid-item">
         <V5 />
       </div>
-    </>
+    </div>
   );
 }
