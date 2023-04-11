a92f0fb9ce918a6504aa3dcec17058646e52a305
Henna <henna.hannila@gmail.com>
2023-04-11 15:30:07 +0300

    custom bäkkäri

--- backend/src/main/java/com/climateapp/backend/repository/CustomViewRepository.java
+++ backend/src/main/java/com/climateapp/backend/repository/CustomViewRepository.java
@@ -1,8 +1,11 @@
 package com.climateapp.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.climateapp.backend.data.CustomView;
 
+@Repository
 public interface CustomViewRepository extends JpaRepository<CustomView, String> {
     
 }
--- backend/src/main/java/com/climateapp/backend/service/CustomViewService.java
+++ backend/src/main/java/com/climateapp/backend/service/CustomViewService.java
@@ -1,9 +1,11 @@
 package com.climateapp.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.climateapp.backend.repository.CustomViewRepository;
 
+@Service
 public class CustomViewService {
     @Autowired
     CustomViewRepository customViewRepository;
