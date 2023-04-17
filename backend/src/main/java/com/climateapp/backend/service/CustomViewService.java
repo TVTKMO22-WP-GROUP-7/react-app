3114ffec2e21fc7ee9f7e6ef62bafdecdc4d192b
Henna <henna.hannila@gmail.com>
2023-04-17 19:24:26 +0300

    merge

--- backend/src/main/java/com/climateapp/backend/service/CustomViewService.java
+++ backend/src/main/java/com/climateapp/backend/service/CustomViewService.java
@@ -11,24 +11,23 @@ public class CustomViewService {
     @Autowired
     CustomViewRepository customViewRepository;
 
-
     public testicustom saveView(
-        String userID,
-        Integer id,
-        Boolean visu1,
-        Boolean visu2,
-        Boolean visu3,
-        Boolean visu4,
-        Boolean visu5,
-        String v1text,
-        String v2text,
-        String v3text,
-        String v4text,
-        String v5text)
-        {
-        testicustom c = new testicustom(id, userID, v1text, v2text, v3text, v4text, v5text, false, false, false, false, false);
+            String userID,
+            Integer id,
+            Boolean visu1,
+            Boolean visu2,
+            Boolean visu3,
+            Boolean visu4,
+            Boolean visu5,
+            String v1text,
+            String v2text,
+            String v3text,
+            String v4text,
+            String v5text) {
+        testicustom c = new testicustom(id, userID, v1text, v2text, v3text, v4text, v5text, false, false, false, false,
+                false);
         customViewRepository.save(c);
         return c;
-}
+    }
 
 }
