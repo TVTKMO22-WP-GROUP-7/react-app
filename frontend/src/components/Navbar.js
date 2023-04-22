4de9cd1175464444adf2d6c0463a91749510bd00
Henna <henna.hannila@gmail.com>
2023-04-22 23:14:47 +0300

    delete account + custom toimii

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,64 +2,71 @@ import React, { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useNavigate, useLocation } from "react-router-dom";
 import "./Navbar.css";
+import NotLoggedNavbar from "./NotLoggedNavbar";
 
 function Navbar() {
-	const navRef = useRef();
-	const navigate = useNavigate()
-	const name = localStorage.getItem("username");
-	const location = useLocation();
+  const navRef = useRef();
+  const navigate = useNavigate();
+  const name = localStorage.getItem("username");
+  const location = useLocation();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav");
-	};
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-	function logout() {
-		localStorage.removeItem("token")
-		localStorage.removeItem("username")
-		navigate("/")
-		window.location.reload(false)
-	}
+  function logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    navigate("/");
+    window.location.reload(false);
+  }
 
-	function changeDefaultView() {
-		if (location.pathname === "/defaultview2") {
-			return "/";
-		} else {
-			return "/defaultview2";
-		}
-	}
+  function changeDefaultView() {
+    if (location.pathname === "/defaultview2") {
+      return "/";
+    } else {
+      return "/defaultview2";
+    }
+  }
 
-	function changeCustomDefaultView() {
-		if (location.pathname === "/custompage") {
-			return "/";
-		} else {
-			return "/custompage";
-		}
-	}
+  function changeCustomDefaultView() {
+    if (location.pathname === "/custompage") {
+      return "/";
+    } else {
+      return "/custompage";
+    }
+  }
 
-
-
-	return (
-		<header>
-			<h3>Current User: {name}</h3>
-			<nav ref={navRef}>
-				<a href={changeDefaultView()}>Change View</a>
-				<a href={changeCustomDefaultView()}>
-					{location.pathname === "/custompage" ? "Default Page" : "Custom Page"}
-				</a>
-				<a href="/changepassword">Change Password</a>
-				<a href="/customviews">Custom views</a>
-				<a href="/" onClick={logout}>Logout</a>
-				<a href="/deleteaccount">Delete account</a>
-				<button className="nav-btn nav-close-btn" onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button
-				className="nav-btn" onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</header>
-	);
+  // Conditionally render the navbar based on whether the user is logged in or not
+  if (name && localStorage.getItem("token")) {
+    return (
+      <header>
+        <h3>Current User: {name}</h3>
+        <nav ref={navRef}>
+          <a href={changeDefaultView()}>Change View</a>
+          <a href={changeCustomDefaultView()}>
+            {location.pathname === "/custompage" ? "Default Page" : "Custom Page"}
+          </a>
+          <a href="/changepassword">Change Password</a>
+          <a href="/customviews">Custom views</a>
+          <a href="/" onClick={logout}>
+            Logout
+          </a>
+          <a href="/deleteaccount">Delete account</a>
+          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={showNavbar}>
+          <FaBars />
+        </button>
+      </header>
+    );
+  } else {
+    return (
+		<NotLoggedNavbar></NotLoggedNavbar>
+    );
+  }
 }
 
 export default Navbar;
